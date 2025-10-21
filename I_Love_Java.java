68a8a4aa521462292a8cb24acedcdf6ddd98d8e9
Srclo <srclo23@126.com>
2025-10-21 18:56:46 +0800

    Add files via upload
    test to upload

---
+++ I_Love_Java.java
@@ -0,0 +1,9 @@
+class I_Love_Java {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; ++ i) {
+            System.out.println("I love Java");
+        }
+        /* WUJUNXUE bc254180 */
+    }
+}
