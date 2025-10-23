30d0b78e9fa5d20863552161908b3071410d1666
Srclo <srclo23@126.com>
2025-10-23 16:11:57 +0800

    Add files via upload

---
+++ Lab1/A_expressions.java
@@ -0,0 +1,9 @@
+class A_expressions {
+    public static void main(String[] args) {
+
+        System.out.println("7.5 X 6.5 - 4.5 X 3 ");
+        System.out.println("--------------------. ");
+        System.out.println("    47.5 - 5.5 ");
+        /* WUJUNXUE bc254180 */
+    }
+}
---
+++ Lab1/BIG_JAVA.java
@@ -0,0 +1,10 @@
+public class BIG_JAVA {
+    public static void main(String[] args) {
+
+        System.out.println("  J");
+        System.out.println("  J  aaa  v   v  aaa");
+        System.out.println("J J  a a   v v   a a");
+        System.out.println("JJJ  aaaa   v    aaaa");
+        /* WUJUNXUE bc254180 */
+    }
+}
