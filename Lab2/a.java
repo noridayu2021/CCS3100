839620261f2f7689061171f02c205bc3f7285da5
Srclo <srclo23@126.com>
2025-10-23 16:51:57 +0800

    Delete Lab2 directory

--- Lab2/a.java
+++
@@ -1,8 +0,0 @@
-public class a {
-    public static void main(String[] args) {
-        double pay = 350;
-        System.out.println("the total amount to pay is:" + pay);
-        System.out.println("and the final price after applying the discount is:" + pay*(1-0.1));
-        // WUJUNXUE bc254180
-    }
-}
