8983211e988968da211e3d0802d89a23b263f4a6
bc251210 <bc251210@student.upm.edu.my>
2025-11-04 15:29:50 +0800

    Add files via upload

---
+++ JavaLover.java
@@ -0,0 +1,14 @@
+package CCS3100;
+
+public class JavaLover {
+	public static void main(String[] args) {
+		//Display the string I Love Java So Much
+		System.out.println("I Love Java!");
+		System.out.println("I Love Java!");
+		System.out.println("I Love Java!");
+		System.out.println("I Love Java!");
+		System.out.println("I Love Java!");
+		
+	}
+
+}
